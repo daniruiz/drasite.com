9833b943c252ed802ac550c0f788e2a05e66b906
Daniel Ruiz de Alegría <daniel@drasite.com>
2019-06-23 20:01:26 +0200

    Improve github banners data update

--- src/content/js/main.js
+++ src/content/js/main.js
@@ -1,55 +1,63 @@
-const extendFunctions = (father, son) => () => {
-  if (typeof father === 'function') father()
-  son()
-}
+!function () {
+  const getGithubAPIInfo = (forceFetch = false) => {
+    if (forceFetch) return fetch('https://api.github.com/users/daniruiz/repos')
+      .then(response => response.json())
+      .then(responseObject => {
+        if (responseObject.message === undefined) {
+          localStorage.setItem('github', JSON.stringify({
+            repos: responseObject,
+            date: new Date
+          }))
+          return responseObject
+        }
+        return {}
+      })
 
-function getGithubApiInfo (fnc) {
-  if (localStorage.getItem('github') === null) {
-    let request = new XMLHttpRequest()
-    request.open('GET', 'https://api.github.com/users/daniruiz/repos')
-    request.onload = () => {
-      if (JSON.parse(request.response).message === undefined) {
-        localStorage.setItem('github', request.response)
-        fnc()
-      }
+    const data = JSON.parse(localStorage.getItem('github')) || {}
+    let yesterday = new Date
+    yesterday.setDate((new Date).getDate() - 1)
+    return new Date(data.date) > yesterday
+      ? Promise.resolve(data)
+      : getGithubAPIInfo(true)
+  }
+
+  const loadProjectLinkGithubInfo = () => {
+    let { repos } = JSON.parse(localStorage.getItem('github'))
+    repos.forEach(function (obj) {
+      let e = $(`#content a[data-github="${obj.name}"]`)
+      if (e !== undefined) e.innerHTML = `
+        <a class="github">
+            <i class="far fa-heart"></i>${obj.stargazers_count}<i class="fas fa-code-branch"></i>${obj.forks}
+        </a>
+        ${e.innerHTML}
+      `
+    })
+  }
+
+  const startGithubBannerLoader = () => {
+    const loadGithubBanner = () => {
+      let e = $('.download-box[data-github]')
+      if (e === undefined) return
+      let repo = e.dataset.github
+      let [data] = JSON.parse(localStorage.getItem('github'))
+        .repos
+        .filter(obj => obj.name === repo ? obj : false)
+      e.innerHTML += `<a class="github" href="${data.html_url}" target="_blank"><i class="far fa-heart"></i>${data.stargazers_count}<i class="fas fa-code-branch"></i>${data.forks}</a>`
     }
-    request.send()
-  } else fnc()
-}
 
-function loadProjectLinkGithubInfo () {
-  let data = JSON.parse(localStorage.getItem('github'))
-  data.forEach(function (obj) {
-    let e = $(`#content a[data-github="${obj.name}"]`)
-    if (e === undefined) return
-    let githubHTML = `<a class="github"><i class="far fa-heart"></i>${obj.stargazers_count}<i class="fas fa-code-branch"></i>${obj.forks}</a>`
-    e.innerHTML = githubHTML + e.innerHTML
-  })
-}
+    loadGithubBanner()
+    __ETHENIS.onLoad = (prevOnLoad => () => {
+      if (typeof prevOnLoad === 'function') prevOnLoad()
+      loadGithubBanner()
+    })(__ETHENIS.onLoad)
+  }
 
-function startGithubBannerLoader () {
-  loadGithubBanner()
-  __ETHENIS.onLoad = extendFunctions(__ETHENIS.onLoad, loadGithubBanner)
-}
-
-function loadGithubBanner () {
-  let e = $('.download-box[data-github]')
-  if (e === undefined) return
-  let repo = e.dataset.github
-  let [data] = JSON.parse(localStorage.getItem('github'))
-    .filter(obj => obj.name === repo ? obj : false)
-  e.innerHTML += `<a class="github" href="${data.html_url}" target="_blank"><i class="far fa-heart"></i>${data.stargazers_count}<i class="fas fa-code-branch"></i>${data.forks}</a>`
-}
-
-{
   window.addEventListener('scroll', () => {
-    scroll = window.scrollY
-    $('header').style.height = 500 - (scroll * 0.5) + 'px'
+    let scroll = window.scrollY
+    $('header').style.height = `${500 - (scroll * 0.5)}px`
 
-    if (window.scrollY > 500)
-      $('#back-to-top').style.display = 'block'
-    else
-      $('#back-to-top').style.display = 'none'
+    if (window.scrollY > 500) $('#back-to-top').style.display = 'block'
+    else $('#back-to-top').style.display = 'none'
   })
 
   $('#back-to-top').onclick = () => {
@@ -66,16 +74,14 @@ function loadGithubBanner () {
 
     let scrollStep = -window.scrollY / (scrollDuration / 15)
     scrollInterval = setInterval(() => {
-      if (window.scrollY != 0)
-        window.scrollBy(0, scrollStep)
-      else
-        stopScrollToTop()
+      if (window.scrollY != 0) window.scrollBy(0, scrollStep)
+      else stopScrollToTop()
     }, 15)
   }
 
-  getGithubApiInfo(() => {
-    loadProjectLinkGithubInfo()
+  getGithubAPIInfo().then(() => {
     startGithubBannerLoader()
+    loadProjectLinkGithubInfo()
   })
-}
+}()
 
