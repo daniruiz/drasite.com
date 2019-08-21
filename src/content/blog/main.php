ea331a4c23cda39087fc3a3ab0bed5af6684894b
Daniel Ruiz de Alegría <daniel@drasite.com>
2019-08-21 13:04:03 +0200

    Add some styling to blog main page

--- src/content/blog/main.php
+++ src/content/blog/main.php
@@ -1,28 +1,82 @@
 <style>
-    header {
-        display: none;
-    }
+    header { display: none; }
 
-    article {
-        max-width: 880px;
+    #blog-container {
+        max-width: 650px;
         padding: 20px;
         margin: auto auto 50px;
         width: 95%;
     }
-</style>
 
-<?php
-    include "content/blog/php/Post.php";
-    $post_files = array_diff(scandir("content/blog/posts"), [".", ".."]);
-    foreach (array_reverse($post_files) as $file) {
-        $post = new Post($file);
-        ?>
+    article > a > h1 { margin: 0 0 15px; }
 
-        <h1><?php echo $post->title ?></h1>
-        <time datetime='<?php echo $post->datetime ?>'><?php echo $post->date_string ?></time>
-        <br/>
+    article > a:hover { text-decoration: none !important; }
 
-        <?php
-        echo $post->content;
+    article > a.read-more-button {
+        line-height: 42px;
+        border-radius: 100px;
+        text-align: center;
+        margin: 30px auto;
+        text-shadow: 0 0 1px white;
+        color: #666;
+        display: block;
+        transition: transform .2s;
+        border: 3px solid #E9E9E9;
+        width: 120px;
     }
-?>
+
+    article > a.read-more-button:hover {
+        transform: scale(1.05);
+        text-decoration: none;
+    }
+
+    article { margin: 50px 0; }
+
+    article time {
+        background-color: #eee;
+        border: solid 1px #ddd;
+        border-radius: 3px;
+    }
+
+    article time::before {
+        content: "";
+        width: 16px;
+        height: 16px;
+        padding: 0;
+        margin: 0 2px 0 0;
+        display: inline-block;
+        vertical-align: middle;
+        background-repeat: no-repeat;
+        background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAAW0lEQVR4AWMgAcyYicZGgKke0x/N/I8Opz+a7AlVAOTigHAF2C0lSQGcBcOkmgAhqWcCpoJnk2Qw9U+SmfkMFrAdM7fBlSCkt83ogJnAClTyDC0UnwHFWBmoAABeoGyUiuDGtQAAAABJRU5ErkJggg==');
+        position: relative;
+        top: -1px;
+    }
+
+    article > p * { color: inherit !important; }
+
+    #blog-container > hr {
+        width: 0;
+        box-shadow: 0 7px rgba(0, 0, 0, .3);
+    }
+</style>
+<div id="blog-container">
+    <?php
+        include "content/blog/php/Post.php";
+        $post_files = array_diff(scandir("content/blog/posts"), [".", ".."]);
+        foreach (array_reverse($post_files) as $file) {
+            $post = new Post($file);
+            ?>
+
+            <article>
+                <a href="/blog/<?php echo $post->title ?>" class="__eth-link"><h1><?php echo $post->title ?></h1></a>
+                <time datetime='<?php echo $post->datetime ?>'><?php echo $post->date_string ?></time>
+                <br/>
+
+                <?php echo $post->preview; ?>
+                <a class="read-more-button __eth-link" href="/blog/<?php echo $post->title ?>">Read more ↦</a>
+            </article>
+            <hr/>
+            <?php
+        }
+    ?>
+</div>
--- src/content/blog/php/Post.php
+++ src/content/blog/php/Post.php
@@ -1,33 +1,40 @@
 <?php
 
-class Post
-{
-    const POSTS_CONTENT_DIR = 'content/blog/posts/';
-    private $file;
-    private $date;
-    public $datetime;
-    public $date_string;
-    public $title;
-    public $content;
-
-    public function __construct($file)
+    class Post
     {
-        $this->file = $file;
-        preg_match('/^(\d{8})-(.+)\.html$/', $this->file, $matches);
-        $this->content = file_get_contents(self::POSTS_CONTENT_DIR . $this->file);
-        list(, $rawDate, $this->title) = $matches;
-        $this->date = new DateTime($rawDate);
-        $this->date_string = $this->date->format('F j, Y');
-        $this->datetime = $this->date->format('Y-m-d');
-    }
+        const POSTS_CONTENT_DIR = 'content/blog/posts/';
+        private $file;
+        private $date;
+        public $datetime;
+        public $date_string;
+        public $title;
+        public $content;
+        public $preview;
 
-    public static function from_pattern($title_pattern)
-    {
-        $matching_files = glob(self::POSTS_CONTENT_DIR . "????????-*$title_pattern*");
-        $matching_files = empty($matching_files)
-            ? scandir(self::POSTS_CONTENT_DIR)
-            : $matching_files;
-        $file = basename(array_pop($matching_files));
-        return new self($file);
+        public function __construct($file)
+        {
+            $this->file = $file;
+            preg_match('/^(\d{8})-(.+)\.html$/', $this->file, $matches);
+            $this->content = file_get_contents(self::POSTS_CONTENT_DIR . $this->file);
+            $document = new DOMDocument();
+            $document->loadHTML('<body>' . $this->content . '</body>');
+            $children = $document->getElementsByTagName('body')->item(0)->childNodes;
+            $this->preview = '';
+            for ($i = 0; $i <= 4; $i++)
+                $this->preview .= $document->saveHTML($children->item($i));
+            list(, $rawDate, $this->title) = $matches;
+            $this->date = new DateTime($rawDate);
+            $this->date_string = $this->date->format('F j, Y');
+            $this->datetime = $this->date->format('Y-m-d');
+        }
+
+        public static function from_pattern($title_pattern)
+        {
+            $matching_files = glob(self::POSTS_CONTENT_DIR . "????????-*$title_pattern*");
+            $matching_files = empty($matching_files)
+                ? scandir(self::POSTS_CONTENT_DIR)
+                : $matching_files;
+            $file = basename(array_pop($matching_files));
+            return new self($file);
+        }
     }
-}
