768a1bdfbb15a0ae73bc85f5025a3c583fa2e91a
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-01-24 12:23:57 +0100

    Add blog twitter share button

--- src/content/blog/blog.php
+++ src/content/blog/blog.php
@@ -1,6 +1,8 @@
 <?php
     include 'content/blog/php/Post.php';
     $POST = Post::from_url(Ethenis::get_path());
+    $_TWITTER_TEXT = urlencode('drasite.com | DяA  ::  '.$POST->title);
+    $_TWITTER_URL = urlencode('https://drasite.com/blog/'.str_replace(' ', '+', $POST->title));
 ?>
 
 <script>
@@ -16,6 +18,9 @@
 <article id="blog-post-container">
     <h1><?php echo $POST->title ?></h1>
     <time datetime="<?php echo $POST->date_time ?>"><?php echo $POST->date_string ?></time>
+    <a id="twitter-button" href="https://twitter.com/intent/tweet?text=<?php echo $_TWITTER_TEXT ?>&url=<?php echo $_TWITTER_URL ?>" target="_blank">
+        <i class="fab fa-twitter"></i>
+    </a>
     <br>
     <?php echo $POST->content ?>
 </article>
