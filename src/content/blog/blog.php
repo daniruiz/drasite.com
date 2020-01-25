19622a7497d3e07c201beaacba5b14c8b57643c6
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-01-25 19:33:58 +0100

    Improve blog share buttons layout for small screens

--- src/content/blog/blog.php
+++ src/content/blog/blog.php
@@ -15,24 +15,25 @@
 <article id="blog-post-container">
     <h1><?php echo $POST->title ?></h1>
     <time datetime="<?php echo $POST->date_time ?>"><?php echo $POST->date_string ?></time>
-    <a class="share-button" href="https://twitter.com/intent/tweet?text=<?php echo $_SHARE_TEXT_ENCODED ?>&url=<?php echo $_SHARE_URL_ENCODED ?>"
-        title="Share on twitter"
-        style="color: #4a88f1"
-        target="_blank">
-        <i class="fab fa-twitter"></i>
-    </a>
-    <a class="share-button" href="https://facebook.com/sharer/sharer.php?t=<?php echo $_SHARE_TEXT_ENCODED ?>&u=<?php echo $_SHARE_URL_ENCODED ?>"
-        title="Share on facebook"
-        style="color: #265ab1"
-        target="_blank">
-        <i class="fab fa-facebook"></i>
-    </a>
-    <a class="share-button"
-        title="Share"
-        style="color: #c64570"
-        id="share-button">
-        <i class="fas fa-share-alt"></i>
-    </a>
+    <span id="share-buttons-wrapper">
+        <a class="share-button fab fa-twitter"
+            href="https://twitter.com/intent/tweet?text=<?php echo $_SHARE_TEXT_ENCODED ?>&url=<?php echo $_SHARE_URL_ENCODED ?>"
+            title="Share on twitter"
+            style="color: #4a88f1"
+            target="_blank">
+        </a>
+        <a class="share-button fab fa-facebook"
+            href="https://facebook.com/sharer/sharer.php?t=<?php echo $_SHARE_TEXT_ENCODED ?>&u=<?php echo $_SHARE_URL_ENCODED ?>"
+            title="Share on facebook"
+            style="color: #265ab1"
+            target="_blank">
+        </a>
+        <a class="share-button fas fa-share-alt"
+            title="Share"
+            style="color: #c64570"
+            id="share-button">
+        </a>
+    </span>
     <br>
     <?php echo $POST->content ?>
 </article>
