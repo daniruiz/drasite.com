9b75f4bc1683e7dab36cac0f4c6e6472086a9ac2
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-03-03 13:46:09 +0100

    Update tetris game

--- src/games/tetris/js/Tetris/tetris.js
+++ src/games/tetris/js/Tetris/tetris.js
@@ -95,9 +95,6 @@
     _updateVisualBoard () {}
 
     _gameOver () {
-      this._lockGame = true
-
-      clearTimeout(this._timer)
       if (this._gameOverCallback)
         this._gameOverCallback()
     }
@@ -168,6 +165,8 @@
       this._addNewPiece()
 
       const timer = () => setTimeout(() => {
+        if (this._lockGame)
+          return
         this._timerCallback()
         this._timer = timer()
       }, this._speed)
@@ -351,6 +350,12 @@
       const pieceTypes = Object.keys(PIECES)
       return pieceTypes[Math.floor(Math.random() * pieceTypes.length)]
     }
+
+    _gameOver () {
+      this._lockGame = true
+      clearTimeout(this._timer)
+      super._gameOver()
+    }
   }
 })(typeof TetrisBoard !== 'undefined'
   ? TetrisBoard
@@ -392,5 +397,91 @@ class WebTetris extends Tetris {
         }
       }
     }
+  }
+}
+/* eslint-disable comma-dangle */
+/* eslint-disable curly */
+
+'use strict'
+
+class WebTetrisClient extends WebTetris {
+  constructor (boardElement, url) {
+    if (!(boardElement instanceof window.Element)) return
+
+    super(boardElement)
+
+    this._playing = false
+    this._lockMessages = false
+
+    this._webSocket = new WebSocket(url)
+    this._webSocket.onerror = e => this._error(e)
+    this._webSocket.onmessage = ({ data }) => {
+      const message = JSON.parse(data)
+      if (message.scores)
+        this.scores = message.scores
+      if (message.instruction)
+        this.execInstruction(message.instruction)
+      if (message.nextPieceType) {
+        this._nextPieceType = message.nextPieceType
+        if (!this._playing)
+          this.start()
+      }
+    }
+  }
+
+  set onError (callback) {
+    if (typeof callback !== 'function')
+      return
+    this._onErroCallback = callback
+  }
+
+  start () {
+    this._addNewPiece()
+    this._playing = true
+  }
+
+  stop () { this._webSocket.close() }
+
+  _timerCallback () {
+    this._lockMessages = true
+    super._timerCallback()
+    this._lockMessages = false
+  }
+
+  movePieceDown () {
+    this._sendData({ instruction: Tetris.INSTRUCTIONS.DOWN })
+    super.movePieceDown()
+  }
+
+  movePieceLeft () {
+    this._sendData({ instruction: Tetris.INSTRUCTIONS.LEFT })
+    super.movePieceLeft()
+  }
+
+  movePieceRight () {
+    this._sendData({ instruction: Tetris.INSTRUCTIONS.RIGHT })
+    super.movePieceRight()
+  }
+
+  rotatePiece () {
+    this._sendData({ instruction: Tetris.INSTRUCTIONS.ROTATE })
+    super.rotatePiece()
+  }
+
+  _sendData (data) {
+    if (this._lockMessages) return
+    this._webSocket.send(JSON.stringify(data))
+  }
+
+  saveScore (name) {
+    if (this.score !== 0)
+      this._sendData({ saveScoreName: name })
+  }
+
+  _error (e) {
+    if (this._onErroCallback)
+      this._onErroCallback()
+    else
+      console.error(e)
   }
 }
---
+++ src/games/tetris/js/tetris-page.js
@@ -0,0 +1,93 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable curly */
+
+const KEYS = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39,
+}
+
+if (location.protocol === 'https:')
+  location.replace(`http:${location.href.substring(location.protocol.length)}`);
+
+const wsUrl = 'ws://34.91.128.107:8080'
+
+let tetris
+window.onload = () => {
+  const tetrisContainer = document.getElementById('tetris')
+  const configureTetris = tetris => {
+    tetris.onGameOver = () => { document.getElementById('game-over').style.display = 'block' }
+    tetris.onInfoChage = info => {
+      document.querySelectorAll('[class^="piece--next"]')
+        .forEach(element => { element.className = `piece--next--${info.nextPieceType}` })
+      document.getElementsByClassName('__tetris-container')[0]
+        .dataset.infoText = `Level ${info.level}  ::  Score ${info.score}`
+    }
+    return tetris
+  }
+  tetris = configureTetris(new WebTetrisClient(tetrisContainer, wsUrl))
+  tetris.onError = () => {
+    document.getElementById('show-scores').style.display = 'none'
+    document.getElementById('save-score-form').style.display = 'none'
+    tetris = configureTetris(new WebTetris(tetrisContainer))
+    tetris.start()
+  }
+
+  document.getElementById('save-score-form').onsubmit = ({ target }) => {
+    tetris.saveScore(target.querySelector('input[type=text]').value)
+  }
+
+  document.getElementById('show-scores').onclick = () => {
+    tetris.stop()
+    const container = Object.assign(document.createElement('DIV'), { id: 'scores' })
+    container.innerHTML = tetris.scores.reduce((code, { name, score }) => {
+      code += `<div><span>${name}</span><span>${score}</span></div>`
+      return code
+    }, '<span class="blinker">&lt;</span> <a href=".">RETURN</a><p>▓▓▒▒░░ SCORES ░░▒▒▓▓</p>')
+    document.body.innerHTML = container.outerHTML
+  }
+
+  const addButtonPressEvent = (element, action) => {
+    const keyPress = (target, action) => {
+      action()
+
+      let interval
+      const timeout = setTimeout(() => {
+        document.getElementById('tetris').classList.add('key-press')
+        interval = setInterval(() => action(), 50)
+      }, 250)
+      target.onmouseup = target.ontouchend = () => {
+        document.getElementById('tetris').classList.remove('key-press')
+        clearInterval(interval)
+        clearTimeout(timeout)
+        target.onmouseup = target.ontouchend = null
+      }
+    }
+    element.ontouchstart = element.onmousedown = event => {
+      event.preventDefault()
+      keyPress(event.target, action)
+    }
+  }
+  addButtonPressEvent(document.getElementsByClassName('control--left')[0], () => tetris.movePieceLeft())
+  addButtonPressEvent(document.getElementsByClassName('control--down')[0], () => tetris.movePieceDown())
+  addButtonPressEvent(document.getElementsByClassName('control--right')[0], () => tetris.movePieceRight())
+  addButtonPressEvent(document.getElementsByClassName('control--action')[0], () => tetris.rotatePiece())
+
+  document.onkeydown = event => {
+    switch (event.keyCode) {
+      case KEYS.UP:
+        tetris.rotatePiece()
+        break
+      case KEYS.DOWN:
+        tetris.movePieceDown()
+        break
+      case KEYS.LEFT:
+        tetris.movePieceLeft()
+        break
+      case KEYS.RIGHT:
+        tetris.movePieceRight()
+        break
+    }
+  }
+}
