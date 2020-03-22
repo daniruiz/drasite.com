a1c42bd64cae616f3c940b877686beec601e2d48
Daniel Ruiz de Alegría <daniel@drasite.com>
2020-03-22 19:13:41 +0100

    Reorganize content files

--- src/content/js/common.js
+++ src/content/js/common.js
@@ -1,2 +1,18 @@
 $$ = query => document.querySelectorAll(query)
 $ = query => document.querySelector(query) || undefined
+
+const updateMetaData = (() => {
+  const defaultTitle = document.title
+  const defaultDescription = $('meta[name=description]').content
+
+  return (obj) => {
+    document.title = obj && obj.title || defaultTitle
+
+    $('meta[name=description]').remove()
+    document.head.appendChild(
+      Object.assign(document.createElement('META'), {
+        name: 'description',
+        content: obj && obj.description || defaultDescription
+      }))
+  }
+})()
