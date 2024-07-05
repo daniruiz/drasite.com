14588b73a650649ef94b8b8afd260fd6d268b785
Daniel Ruiz de Alegría <daniel@drasite.com>
2024-07-05 15:22:56 +0200

    Add new google analytics tag

--- src/content/main.php
+++ src/content/main.php
@@ -27,6 +27,16 @@
 
         <!-- Minimum CSS styles -->
         <style><?php include "content/css/min.css" ?></style>
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-G975NQNE8M"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-G975NQNE8M');
+        </script>
     </head>
     <body>
         <header></header>
